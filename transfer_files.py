a7dde37b681560e0517ad12c85b41551eef0e9dd
yo-cleon <cleon.form@gmail.com>
2021-06-22 22:34:40 +0200

    Se añade un fichero de configuracion para las variables del sistema (#4)

--- transfer_files.py
+++ transfer_files.py
@@ -5,18 +5,27 @@ import socket
 import paramiko
 from paramiko.ssh_exception import NoValidConnectionsError, BadHostKeyException, AuthenticationException, SSHException
 from scp import SCPClient
+from configparser import ConfigParser
 
 print("Empieza script")
 
-'''
-Establecemos la conexión con el servidor para
-trasferir los archivos.
-'''
-server = '192.168.1.164'
-port = 22
-user = secrets.user
-password = secrets.password
-def createSSHClient(server, port, user, password):
+# Leer fichero de configuracion
+config_object = ConfigParser()
+config_object.read("config.ini")
+serverinfo = config_object["SERVERCONFIG"]
+server_host = serverinfo["host"]
+server_ip = serverinfo["server_ip"]
+server_port = serverinfo["port"]
+dst_folder = serverinfo["destination_folder"]
+hostinfo = config_object["HOSTCONFIG"]
+src_folder = hostinfo["source_folder"]
+
+# Establecemos la conexión con el servidor para la transferencia de archivos
+def createSSHClient():
+    server = server_ip
+    port = 22
+    user = secrets.user
+    password = secrets.password
     client = paramiko.SSHClient()
     client.load_system_host_keys()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
@@ -30,21 +39,18 @@ def createSSHClient(server, port, user, password):
         exit(1)
 
 
-'''
-Iniciamos el proceso de transferencia, comprobando si el directorio imgs
-contiene algún archivo a transferir. 
-'''
-folder = os.listdir('./imgs')
+# Si hay imagenes en el directorio origen se inicia el proceso de transferencia
+folder = os.listdir(src_folder)
 if len(folder) == 0:
-    print("Directory is empty")
+    print("El directorio está vacío")
 else:
     print("Conectamos con el cliente")
-    ssh = createSSHClient(server, port, user, password)
+    ssh = createSSHClient()
     scp = SCPClient(ssh.get_transport())
-    print("Empieza el traspaso")
-    folder = os.scandir('./imgs')
+    print("Empieza la transferencia de archivos")
+    folder = os.scandir(src_folder)
     for file in folder:
         print(file.name)
-        scp.put(file, './Pictures/')
+        scp.put(file, dst_folder)
 
 print("Finaliza el script")
