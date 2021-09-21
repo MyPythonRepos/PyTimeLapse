9e9cbd7afc5bec4bb54feaef70de2057075970af
yo-cleon <cleon.form@gmail.com>
2021-09-21 21:18:56 +0200

    Se actualiza el codigo para dividirlo en funciones

--- transfer_files.py
+++ transfer_files.py
@@ -1,54 +1,63 @@
-import secrets
 import os
-import socket
 import paramiko
-from sys import exit
+import secrets
+import socket
+from configparser import ConfigParser
 from paramiko.ssh_exception import NoValidConnectionsError, BadHostKeyException, AuthenticationException, SSHException
 from scp import SCPClient
-from configparser import ConfigParser
+from sys import exit
 
-print("Empieza script")
 
 # Leer fichero de configuracion
-config_object = ConfigParser()
-config_object.read("config.ini")
-serverinfo = config_object["SERVERCONFIG"]
-server_host = serverinfo["host"]
-server_ip = serverinfo["server_ip"]
-server_port = serverinfo["port"]
-dst_folder = serverinfo["destination_folder"]
-hostinfo = config_object["HOSTCONFIG"]
-src_folder = hostinfo["source_folder"]
+def leer_configuracion():
+  config_object = ConfigParser()
+  config_object.read("config.ini")
+  serverinfo = config_object["SERVERCONFIG"]
+  server_host = serverinfo["host"]
+  server_ip = serverinfo["server_ip"]
+  server_port = serverinfo["port"]
+  dst_folder = serverinfo["destination_folder"]
+  hostinfo = config_object["HOSTCONFIG"]
+  src_folder = hostinfo["source_folder"]
+  return (server_host, server_ip, server_port, dst_folder, src_folder)
+
 
 # Establece conexión con el servidor para la transferencia de archivos
-def createSSHClient():
-    client = paramiko.SSHClient()
-    client.load_system_host_keys()
-    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    try:
-        client.connect(server_ip, server_port, secrets.user, secrets.password, timeout=10)
-        print("Conexión realizada")
-        return client
-    except (NoValidConnectionsError, BadHostKeyException, AuthenticationException, SSHException, socket.error) as e:
-        print("Hay un error")
-        print(e)
-        exit(1)
+def createSSHClient(server_ip, server_port):
+  client = paramiko.SSHClient()
+  client.load_system_host_keys()
+  client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+  try:
+    client.connect(server_ip, server_port, secrets.user, secrets.password, timeout=10)
+    print("Conexión realizada")
+    return client
+  except (NoValidConnectionsError, BadHostKeyException, AuthenticationException, SSHException, socket.error) as e:
+    print("Hay un error")
+    print(e)
+    exit(1)
 
 
 # Si hay imagenes en el directorio origen se inicia el proceso de transferencia
-folder = os.listdir(src_folder)
-if len(folder) == 0:
+def transferir_archivos(server_host, server_ip, server_port, dst_folder, src_folder):
+  folder = os.listdir(src_folder)
+  if len(folder) == 0:
     print("El directorio está vacío")
-else:
+  else:
     print("Conectamos con el cliente")
-    ssh = createSSHClient()
+    ssh = createSSHClient(server_ip, server_port)
     scp = SCPClient(ssh.get_transport())
     print("Empieza la transferencia de archivos")
-    folder = os.scandir(src_folder)
     scp.put(src_folder, recursive=True, remote_path=dst_folder)
-   # for file in folder:
-   #     print("Fichero transferido: " + file.name)
-   #     scp.put(file, remote_path=dst_folder)
-   #     os.remove(file) 
+    # Se vacía el directorio de las imagenes
+    for file in os.listdir(src_folder):
+      os.remove(src_folder + '/' + file)
 
-print("Finaliza el script")
+def main():
+  print("Empieza script")
+  server_host, server_ip, server_port, dst_folder, src_folder = leer_configuracion()
+  transferir_archivos(server_host, server_ip, server_port, dst_folder, src_folder)
+  print("Finaliza el script")
+
+
+if __name__ == '__main__':
+  main()
